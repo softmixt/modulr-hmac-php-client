778369b45683dbd0a0231662d87053581cd479d2
Stewart McIntosh <stewis@gmail.com>
2017-03-07 16:53:22 +0000

    test api must right real tests….

--- src/Modulr.php
+++ src/Modulr.php
@@ -9,13 +9,24 @@
 namespace CrowdProperty\ModulrHmacPhpClient;
 
 
+use CrowdProperty\ModulrHmacPhpClient\Model\Customer;
+
 class Modulr
 {
     protected $request;
 
     public function test()
     {
-        $request = new Request();
-        $request->send();
+        $request = new ModulrApi();
+        $customer = new Customer();
+
+        $customer->setName('test');
+
+        echo $customer;
+
+        $customerApi = $request->customers();
+
+        $customerApi->createCustomer($customer->__toString());
+                //$request->retry();
     }
 }
